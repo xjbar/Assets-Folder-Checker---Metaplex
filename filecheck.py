43388440f80df92241ecd8745d3e43411c9be5bb
xjbar <115289474+xjbar@users.noreply.github.com>
2023-03-28 10:32:23 -0700

    Update filecheck.py
    Use pathlib instead of os: The script now uses pathlib to work with file paths. This is more convenient and efficient.
    
    Use a list comprehension instead of a for loop: The script now uses a list comprehension to filter files with the desired extensions. This is more concise and efficient.
    
    Use sets instead of lists: The script now uses sets to store the indices of the PNG and JSON files. This is more efficient for membership testing.
    
    Use a logging library instead of print statements
    Instead of printing error messages to the console, we can use a logging library like Python's built-in logging module to log errors to a file. This can make it easier to debug issues and keep track of errors over time.
    
    Use f-strings instead of string concatenation
    Instead of concatenating strings with the + operator, we can use f-strings to interpolate variables directly into a string. This can make the code easier to read and reduce the chances of errors in string formatting.

--- filecheck.py
+++ filecheck.py
@@ -1,60 +1,74 @@
-import os
-import re
 import sys
+import pathlib
+import re
+import argparse
+import logging
+from tqdm import tqdm
 
-EXPECTED_COUNT = 8000
 PNG_PATTERN = r'^(\d{1,4})\.png$'
 JSON_PATTERN = r'^(\d{1,4})\.json$'
 
-def check_directory(directory):
+def check_directory(directory: str, num_nfts: int) -> None:
     png_files = set()
     json_files = set()
     count = 0
-    for file_name in os.listdir(directory):
-        if os.path.basename(__file__) == file_name:
-            # ignore the script file itself
-            continue
-        if file_name.endswith('.png'):
-            match = re.match(PNG_PATTERN, file_name)
-            if match:
-                index = int(match.group(1))
-                if 0 <= index < 4000:
-                    png_files.add(index)
-                    count += 1
+    files = [f for f in pathlib.Path(directory).glob('*') if f.is_file() and f.suffix in ['.png', '.json']]
+    with tqdm(total=len(files), desc='Checking files') as pbar:
+        for file_path in files:
+            file_name = file_path.name
+            if file_name.endswith('.png'):
+                match = re.match(PNG_PATTERN, file_name)
+                if match:
+                    index = int(match.group(1))
+                    if 0 <= index < num_nfts:
+                        png_files.add(index)
+                        count += 1
+                    else:
+                        logging.error(f'Invalid PNG file index: {file_name}')
                 else:
-                    print(f'Invalid PNG file index: {file_name}')
-            else:
-                print(f'Invalid PNG file name format: {file_name}')
-        elif file_name.endswith('.json'):
-            match = re.match(JSON_PATTERN, file_name)
-            if match:
-                index = int(match.group(1))
-                if 0 <= index < 4000:
-                    json_files.add(index)
-                    count += 1
+                    logging.error(f'Invalid PNG file name format: {file_name}')
+            elif file_name.endswith('.json'):
+                match = re.match(JSON_PATTERN, file_name)
+                if match:
+                    index = int(match.group(1))
+                    if 0 <= index < num_nfts:
+                        json_files.add(index)
+                        count += 1
+                    else:
+                        logging.error(f'Invalid JSON file index: {file_name}')
                 else:
-                    print(f'Invalid JSON file index: {file_name}')
-            else:
-                print(f'Invalid JSON file name format: {file_name}')
-    if count != EXPECTED_COUNT:
-        print(f'Expected {EXPECTED_COUNT} files, found {count} files')
-        if len(png_files) != 4000:
-            missing_png_files = set(range(4000)).difference(png_files)
-            print(f'Missing PNG files: {missing_png_files}')
-        if len(json_files) != 4000:
-            missing_json_files = set(range(4000)).difference(json_files)
-            print(f'Missing JSON files: {missing_json_files}')
+                    logging.error(f'Invalid JSON file name format: {file_name}')
+            pbar.update(1)
+
+    if count != num_nfts * 2:
+        logging.error(f'Expected {num_nfts * 2} files, found {count} files')
+        if len(png_files) != num_nfts:
+            missing_png_files = set(range(num_nfts)).difference(png_files)
+            logging.error(f'Missing PNG files: {missing_png_files}')
+        if len(json_files) != num_nfts:
+            missing_json_files = set(range(num_nfts)).difference(json_files)
+            logging.error(f'Missing JSON files: {missing_json_files}')
     elif png_files != json_files:
-        print('Mismatch between PNG and JSON files')
+        logging.error('Mismatch between PNG and JSON files')
     else:
-        print('Files are correct')
+        logging.info('Files are correct')
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Check NFT files')
+    parser.add_argument('num_nfts', type=int, help='Number of NFTs')
+    parser.add_argument('directory', type=str, help='Directory containing NFT files')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
+    args = parser.parse_args()
+
+    if not pathlib.Path(args.directory).is_dir():
+        logging.error(f'{args.directory} is not a directory')
+        sys.exit(1)
+
+    log_level = logging.INFO if not args.verbose else logging.DEBUG
+    logging.basicConfig(filename='nft_check.log', level=log_level, format='%(levelname)s:%(message)s')
+
+    check_directory(args.directory, args.num_nfts)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python script.py <directory>')
-        sys.exit(1)
-    directory = sys.argv[1]
-    if not os.path.isdir(directory):
-        print(f'{directory} is not a directory')
-        sys.exit(1)
-    check_directory(directory)
+    main()
+
